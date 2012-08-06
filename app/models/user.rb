6bef08d75d47e4f8ff4ea1db6f4d9adb84490b7b
nviarnes <arcrutus@gmail.com>
2012-08-05 19:12:57 -0700

    finish sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
   
@@ -24,4 +25,11 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   has_many :microposts
+
+
+  private
+
+  	def create_remember_token
+    	self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
